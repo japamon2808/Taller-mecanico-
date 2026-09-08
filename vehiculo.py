d722315b6ec80ce12c1ed4da038dd5f37e0f54d1
Talleres Puente Alto <tallerespa@inacapmail.cl>
2026-09-07 21:30:38 -0300

    Agregar clase base Vehiculo en vehiculo.py

---
+++ vehiculo.py
@@ -0,0 +1,4 @@
+# Definición de la clase Vehiculo (siguiendo la convención PEP 8 de nombres de clase con mayúscula inicial)
+class Vehiculo:
+    # Se utiliza la instrucción 'pass' para permitir que la clase esté vacía por ahora, sin atributos ni métodos
+    pass
